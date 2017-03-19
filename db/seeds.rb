f89bf6d6ccec32e49f82eb9b6367c10a5fb4f68c
Charles Painter <cpainter85@gmail.com>
2017-03-19 17:46:57 -0400

    Default Round and Category seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Round.destroy_all
+
+Round.create(name: "Regular", number_of_rounds: 8)
+Round.create(name: "Semi-Final", number_of_rounds: 1)
+Round.create(name: "Final", number_of_rounds: 1)
+
+Category.destroy_all
+Category.create(name: "Athletes", minimum: 4, maximum: 8)
+Category.create(name: "Sports Moments", minimum: 3, maximum: 5)
+Category.create(name: "Sports Teams", minimum: 2, maximum: 2)
+Category.create(name: "FLEX", minimum: 2, maximum: 2)
