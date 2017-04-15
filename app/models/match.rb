6ad1981a70728e5b8c6961336eb5794e4f3e650c
Charles Painter <cpainter85@gmail.com>
2017-04-15 16:02:47 -0400

    Adds #complete and #votes_for_team methods to match

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,9 +1,21 @@
 class Match < ApplicationRecord
+  has_many :votes
+
+  def complete?
+    User.all
+      .reject { |user| user.match_participant?(self) }
+      .none? { |user| user.has_not_voted_on_match?(self) }
+  end
+
   def team1
     User.includes(:draft_picks).find(team1_id)
   end
 
   def team2
     User.includes(:draft_picks).find(team2_id)
+  end
+
+  def votes_for_team(team)
+    self.votes.where(vote_for_team_id: team.id).size
   end
 end
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -3,7 +3,33 @@ require 'rails_helper'
 describe Match do
   let(:user1) { create(:user) }
   let(:user2) { create(:user) }
-  let(:match) { build(:match, team1_id: user1.id, team2_id: user2.id) }
+  let(:match) { create(:match, team1_id: user1.id, team2_id: user2.id) }
+
+  describe 'relationships' do
+    it { should have_many(:votes) }
+  end
+
+  describe '#complete?' do
+    let(:eligible_voters) { create_list(:user, 4) }
+
+    subject { match.complete? }
+
+    context "match is complete" do
+      before :each do
+        eligible_voters.each { |voter| voter.votes.create(match_id: match.id, vote_for_team_id: match.team1_id ) }
+      end
+
+      it { is_expected.to eq true }
+    end
+
+    context "match is incomplete" do
+      before :each do
+        eligible_voters.first.votes.create(match_id: match.id, vote_for_team_id: match.team2_id)
+      end
+
+      it { is_expected.to eq false }
+    end
+  end
 
   describe '#team1' do
     it "returns the team with the corresponding team1_id" do
@@ -14,6 +40,19 @@ describe Match do
   describe '#team2' do
     it "returns the team with the corresponding team2_id" do
       expect(match.team2).to eq user2
+    end
+  end
+
+  describe "votes_for_team" do
+    subject { match.votes_for_team(user1) }
+
+    it "returns the votes for the given team" do
+      create(:user).votes.create(match_id: match.id, vote_for_team_id: user1.id)
+      expect(subject).to eq 1
+    end
+
+    context "No votes for the team" do
+      it { is_expected.to eq 0 }
     end
   end
 end
