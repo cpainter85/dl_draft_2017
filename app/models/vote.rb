329c520a00d4d6c38a3d8d258740d13a042bcdad
Charles Painter <cpainter85@gmail.com>
2017-05-14 14:56:33 -0400

    Do not allow user to vote on a match they are a participant in

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,11 @@
 class Vote < ApplicationRecord
+  belongs_to :match
+  belongs_to :user
+
   validates_uniqueness_of :user_id, scope: :match_id
+  validate :user_cannot_be_match_participant
+
+  def user_cannot_be_match_participant
+    errors.add(:user_id, "user can't be a match participant") if user.match_participant?(match)
+  end
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,5 +3,20 @@ require 'rails_helper'
 describe Vote do
   describe 'validations' do
     it { should validate_uniqueness_of(:user_id).scoped_to(:match_id) }
+
+    describe '#user_cannot_be_match_participant' do
+      let(:user) { create(:user) }
+      let(:match) { create(:match, team1_id: user.id, team2_id: create(:user).id) }
+
+      it "validates that user is not a match participant" do
+        vote = match.votes.create(user_id: user.id, vote_for_team_id: match.team1_id)
+        expect(vote.valid?).to eq false
+      end
+    end
+  end
+
+  describe 'relationships' do
+    it { should belong_to(:match) }
+    it { should belong_to(:user) }
   end
 end
